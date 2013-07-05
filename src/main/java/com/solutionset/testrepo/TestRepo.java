0b54d532a6b013667160790e3b86512c1a206a1a
atrissel <atrissel@solutionset.com>
2013-07-05 14:27:44 -0700

    fixed import

---
+++ src/main/java/com/solutionset/testrepo/TestRepo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.solutionset.testrepo;
+
+import org.jsoup.parser.*;
+
+/**
+ * @author adamtrissel
+ *
+ */
+public class TestRepo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
